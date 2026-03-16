af5dd2b8a6eb92b258c629b7c737ddac5d974c39
Donovan McGilbray <djaymcgilbray@gmail.com>
2026-03-16 09:51:37 -0700

    created styling for app

--- stock-tracker/src/App.js
+++ stock-tracker/src/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./components/SearchBar";
 import StockCard from "./components/StockCard";
 import StockChart from "./components/StockChart";
 import { fetchCurrentStock, fetchHistoricalStock } from "./services/stockApi";
+import "./styles/App.css";
 
 function App() {
   const [stock, setStock] = useState(null);
@@ -24,11 +25,13 @@ function App() {
       <SearchBar onSearch = {handleSearch} />
       {loading && <p className = "loading">Loading stock data...</p>}
       {stock && <StockCard stock = {stock} />}
-      {chartData.length > 0 && (
+      {chartData && chartData.length > 0 ? (
         <div className = "chart-container">
           <StockChart data = {chartData} />
         </div>
-      )}
+      ) : stock && !loading ? (
+        <p className = "loading">Historical chart not available.</p>
+      ) : null}
     </div>
   );
 }
